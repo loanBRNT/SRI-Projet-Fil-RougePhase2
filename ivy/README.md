568f26e16c9f4368cbec54d119d3b3874387183b
BERNAT Loan <loan.bernat@univ-tlse3.fr>
2022-02-15 14:21:18 +0100

    J'essaye d'init IVY

---
+++ ivy/README.md
@@ -0,0 +1,11 @@
+# Ivy
+
+[Présentation Ivy](./Presentation%20Ivy.pdf) 
+
+## Installer Ivy [Java]
+
+[Tutoriel pas à pas](./Installer%20Ivy%20%5BJava%5D.pdf)  
+
+## Installer Ivy [C]
+
+[Tutoriel pas à pas](./Installer%20Ivy%20%5BC%5D.md) 
--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,11 @@
-import processing.core.PApplet;
-import processing.core.PImage;
 
-public class Main extends PApplet{
+public class Main{
     //Attributs
-    public static PApplet processing;
+
     //Constructeur
 
     //Méthodes
     public static void main(String[] args) {
-        PApplet.main("Main",args);
-    }
-
-    public void settings(){
-        size(900, 600);
-    }
-
-    public void setup(){
-        processing = this;
-        surface.setTitle("Processing on IntellijIDEA");
-        System.out.println("L'image est affichée");
-        colorMode(HSB,255,255,255);
-    }
-
-    public void draw(){
-        PImage imageChaudasse = loadImage("../Chaudasse.png");
-
-        image(imageChaudasse,0,0);
-
-
-
+        System.out.println("Hello World !");
     }
 }
