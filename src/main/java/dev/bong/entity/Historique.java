8304f74be45ad383269d89bddd025b03aae1e066
Samissier <samuel.missier@univ-tlse3.fr>
2022-03-28 16:20:30 +0200

    MàJ Historique

--- src/main/java/dev/bong/entity/Historique.java
+++ src/main/java/dev/bong/entity/Historique.java
@@ -2,14 +2,19 @@ package dev.bong.entity;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class Historique {
     public static void ecrire(TypeRequete type, String texte) {
         try {
+            DateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+            Calendar cal = Calendar.getInstance();
             File f = new File("./Historique.txt");
             f.createNewFile();
             FileWriter fw = new FileWriter(f, true);
-            fw.write(type.toString() + " : " + texte + "\n");
+            fw.write(format.format(cal.getTime())+" - "+type.toString()+" : "+texte+"\n");
             fw.flush();
             fw.close();
         } catch (IOException ex) {
